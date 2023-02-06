049f3cc52b3fc19ba18dafb78ea4f4933a528dfa
Alaa Alser <alaalsser@gmail.com>
2023-02-07 00:38:56 +0200

    add register page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -2,6 +2,7 @@ import styles from "./auth.module.scss";
 import loginImg from "../../assets/login.png";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
+import { Card } from "../../components";
 
 const Login = () => {
   return (
@@ -15,26 +16,28 @@ const Login = () => {
           width={400}
         />
       </div>
-      <div className={styles.form}>
-        <h2>Login</h2>
-        <form>
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <button className="--btn --btn-primary --btn-block">Login</button>
-          <div className={styles.links}>
-            <Link>Forgot Password?</Link>
-          </div>
-        </form>
-        <button className="--btn --btn-danger --btn-block">
-          <FaGoogle />
-          Login with Google
-        </button>
-        <span className={styles.register}>
-          <p>
-            Don't have an account? <Link to="/register">Register</Link>
-          </p>
-        </span>
-      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Login</h2>
+          <form>
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <button className="--btn --btn-primary --btn-block">Login</button>
+            <div className={styles.links}>
+              <Link>Forgot Password?</Link>
+            </div>
+          </form>
+          <button className="--btn --btn-danger --btn-block">
+            <FaGoogle />
+            Login with Google
+          </button>
+          <span className={styles.register}>
+            <p>
+              Don't have an account? <Link to="/register">Register</Link>
+            </p>
+          </span>
+        </div>
+      </Card>
     </section>
   );
 };
