9515095803c668f2b42ee253df2ab70c3e3bb239
Alaa Alser <alaalsser@gmail.com>
2023-02-07 04:46:56 +0200

    view prodcuts admin

--- src/components/adminPrivateRoute/AdminPrivateRoute.js
+++ src/components/adminPrivateRoute/AdminPrivateRoute.js
@@ -10,18 +10,7 @@ const AdminPrivateRoute = ({ children }) => {
   if (userEmail === "test@gmail.com") {
     return children;
   }
-  return (
-    <section style={{ height: "80vh" }}>
-      <div className="container">
-        <h2>Permission Denied.</h2>
-        <p>This page can only be view by an Admin user.</p>
-        <br />
-        <Link to="/">
-          <button className="--btn">&larr; Back To Home</button>
-        </Link>
-      </div>
-    </section>
-  );
+  return null;
 };
 
 export const AdminOnlyLink = ({ children }) => {
