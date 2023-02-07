f99082675e79066078fb2bd82dd6289d9861d22a
Alaa Alser <alaalsser@gmail.com>
2023-02-07 04:29:08 +0200

    create admin navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Contact, Login, Register, RestPassword, Admin } from "./pages";
 import { Header, Footer } from "./components";
 import { ToastContainer } from "react-toastify";
-import AdminPrivateRoute, {
-  AdminOnlyLink,
-} from "./components/adminPrivateRoute/AdminPrivateRoute";
+import AdminPrivateRoute from "./components/adminPrivateRoute/AdminPrivateRoute";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
